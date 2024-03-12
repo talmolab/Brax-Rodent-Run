de4483891cad2d822c424f1e705b92dee219172a
scott-yj-yang <scottyang177@gmail.com>
2024-03-12 06:03:49 +0000

    adapt to the brax api

--- brax_rodent_run_ppo.py
+++ brax_rodent_run_ppo.py
@@ -21,7 +21,7 @@ from brax import base
 from brax import envs
 from brax import math
 from brax.base import Base, Motion, Transform
-from brax.envs.base import Env, MjxEnv, State
+from brax.envs.base import Env, PipelineEnv, State
 from brax.mjx.base import State as MjxState
 from brax.training.agents.ppo import train as ppo
 from brax.training.agents.ppo import networks as ppo_networks
@@ -59,133 +59,132 @@ def load_params(param_path: Text) -> Dict:
 
 params = load_params("params/params.yaml")
 
-class Rodent(MjxEnv):
-    
-    # Might want to change the terminate_when_unhealthy params to enables
-    # longer episode length, since the average episode length is too short (1 timestep)
-    # temp change the `terminate_when_unhealthy` to extend the episode length.
-    def __init__(
-            self,
-            forward_reward_weight=5,
-            ctrl_cost_weight=0.1,
-            healthy_reward=0.5,
-            terminate_when_unhealthy=False,
-            healthy_z_range=(0.2, 1.0),
-            reset_noise_scale=1e-2,
-            exclude_current_positions_from_observation=False,
-            **kwargs,
-    ):
-        params = load_params("params/params.yaml")
-        mj_model = mujoco.MjModel.from_xml_path(params["XML_PATH"])
-        mj_model.opt.solver = mujoco.mjtSolver.mjSOL_CG
-        mj_model.opt.iterations = 6
-        mj_model.opt.ls_iterations = 6
+#@title Humanoid Env
 
-        physics_steps_per_control_step = 5
-        kwargs['n_frames'] = kwargs.get(
-            'n_frames', physics_steps_per_control_step)
+#@title Humanoid Env
 
-        super().__init__(model=mj_model, **kwargs)
+class Rodent(PipelineEnv):
 
-        self._forward_reward_weight = forward_reward_weight
-        self._ctrl_cost_weight = ctrl_cost_weight
-        self._healthy_reward = healthy_reward
-        self._terminate_when_unhealthy = terminate_when_unhealthy
-        self._healthy_z_range = healthy_z_range
-        self._reset_noise_scale = reset_noise_scale
-        self._exclude_current_positions_from_observation = (
-            exclude_current_positions_from_observation
-        )
+  def __init__(
+      self,
+      forward_reward_weight=1.25,
+      ctrl_cost_weight=0.1,
+      healthy_reward=5.0,
+      terminate_when_unhealthy=True,
+      healthy_z_range=(0.70, 0.0),
+      reset_noise_scale=1e-2,
+      exclude_current_positions_from_observation=True,
+      **kwargs,
+  ):
+    params = load_params("params/params.yaml")
+    mj_model = mujoco.MjModel.from_xml_path(params["XML_PATH"])
+    mj_model.opt.solver = mujoco.mjtSolver.mjSOL_CG
+    mj_model.opt.iterations = 6
+    mj_model.opt.ls_iterations = 6
 
-    def reset(self, rng: jp.ndarray) -> State:
-        """Resets the environment to an initial state."""
-        rng, rng1, rng2 = jax.random.split(rng, 3)
+    sys = mjcf.load_model(mj_model)
 
-        low, hi = -self._reset_noise_scale, self._reset_noise_scale
-        qpos = self.sys.qpos0 + jax.random.uniform(
-            rng1, (self.sys.nq,), minval=low, maxval=hi
-        )
-        qvel = jax.random.uniform(
-            rng2, (self.sys.nv,), minval=low, maxval=hi
-        )
+    physics_steps_per_control_step = 5
+    kwargs['n_frames'] = kwargs.get(
+        'n_frames', physics_steps_per_control_step
+    )
+    kwargs['backend'] = 'mjx'
 
-        data = self.pipeline_init(qpos, qvel)
+    super().__init__(sys, **kwargs)
 
-        obs = self._get_obs(data.data, jp.zeros(self.sys.nu))
-        reward, done, zero = jp.zeros(3)
-        metrics = {
-            'forward_reward': zero,
-            'reward_linvel': zero,
-            'reward_quadctrl': zero,
-            'reward_alive': zero,
-            'x_position': zero,
-            'y_position': zero,
-            'distance_from_origin': zero,
-            'x_velocity': zero,
-            'y_velocity': zero,
-        }
-        return State(data, obs, reward, done, metrics)
+    self._forward_reward_weight = forward_reward_weight
+    self._ctrl_cost_weight = ctrl_cost_weight
+    self._healthy_reward = healthy_reward
+    self._terminate_when_unhealthy = terminate_when_unhealthy
+    self._healthy_z_range = healthy_z_range
+    self._reset_noise_scale = reset_noise_scale
+    self._exclude_current_positions_from_observation = (
+        exclude_current_positions_from_observation
+    )
 
-    def step(self, state: State, action: jp.ndarray) -> State:
-        """Runs one timestep of the environment's dynamics."""
-        data0 = state.pipeline_state
-        data = self.pipeline_step(data0, action)
-        # based on the timestep simulation, calculate the rewards
-        com_before = data0.data.subtree_com[1]
-        com_after = data.data.subtree_com[1]
-        velocity = (com_after - com_before) / self.dt
-        forward_reward = self._forward_reward_weight * velocity[0]
+  def reset(self, rng: jp.ndarray) -> State:
+    """Resets the environment to an initial state."""
+    rng, rng1, rng2 = jax.random.split(rng, 3)
 
-        min_z, max_z = self._healthy_z_range
-        is_healthy = jp.where(data.q[2] < min_z, 0.0, 1.0)
-        is_healthy = jp.where(data.q[2] > max_z, 0.0, is_healthy)
-        if self._terminate_when_unhealthy:
-            healthy_reward = self._healthy_reward
-        else:
-            healthy_reward = self._healthy_reward * is_healthy
+    low, hi = -self._reset_noise_scale, self._reset_noise_scale
+    qpos = self.sys.qpos0 + jax.random.uniform(
+        rng1, (self.sys.nq,), minval=low, maxval=hi
+    )
+    qvel = jax.random.uniform(
+        rng2, (self.sys.nv,), minval=low, maxval=hi
+    )
 
-        ctrl_cost = self._ctrl_cost_weight * jp.sum(jp.square(action))
+    data = self.pipeline_init(qpos, qvel)
 
-        obs = self._get_obs(data.data, action)
-        reward = forward_reward + healthy_reward - ctrl_cost
-        # terminates when unhealthy
-        done = 1.0 - is_healthy if self._terminate_when_unhealthy else 0.0
-        state.metrics.update(
-            forward_reward=forward_reward,
-            reward_linvel=forward_reward,
-            reward_quadctrl=-ctrl_cost,
-            reward_alive=healthy_reward,
-            x_position=com_after[0],
-            y_position=com_after[1],
-            distance_from_origin=jp.linalg.norm(com_after),
-            x_velocity=velocity[0],
-            y_velocity=velocity[1],
-        )
-        print(state.metrics)
-        return state.replace(
-            pipeline_state=data, obs=obs, reward=reward, done=done
-        )
+    obs = self._get_obs(data, jp.zeros(self.sys.nu))
+    reward, done, zero = jp.zeros(3)
+    metrics = {
+        'forward_reward': zero,
+        'reward_linvel': zero,
+        'reward_quadctrl': zero,
+        'reward_alive': zero,
+        'x_position': zero,
+        'y_position': zero,
+        'distance_from_origin': zero,
+        'x_velocity': zero,
+        'y_velocity': zero,
+    }
+    return State(data, obs, reward, done, metrics)
 
-    # this get observation might be faulty?
-    def _get_obs(
-            self, data: mjx.Data, action: jp.ndarray
-    ) -> jp.ndarray:
-        """Observes humanoid body position, velocities, and angles."""
-        position = data.qpos
-        if self._exclude_current_positions_from_observation:
-            position = position[2:]
-        
-        # external_contact_forces are excluded
-        return jp.concatenate([
-            position,
-            data.qvel,
-            data.qfrc_actuator,
-        ])
+  def step(self, state: State, action: jp.ndarray) -> State:
+    """Runs one timestep of the environment's dynamics."""
+    data0 = state.pipeline_state
+    data = self.pipeline_step(data0, action)
 
+    com_before = data0.subtree_com[1]
+    com_after = data.subtree_com[1]
+    velocity = (com_after - com_before) / self.dt
+    forward_reward = self._forward_reward_weight * velocity[0]
 
-# ## training loop
+    min_z, max_z = self._healthy_z_range
+    is_healthy = jp.where(data.q[2] < min_z, 0.0, 1.0)
+    if self._terminate_when_unhealthy:
+      healthy_reward = self._healthy_reward
+    else:
+      healthy_reward = self._healthy_reward * is_healthy
 
-# In[5]:
+    ctrl_cost = self._ctrl_cost_weight * jp.sum(jp.square(action))
+
+    obs = self._get_obs(data, action)
+    reward = forward_reward + healthy_reward - ctrl_cost
+    done = 1.0 - is_healthy if self._terminate_when_unhealthy else 0.0
+    state.metrics.update(
+        forward_reward=forward_reward,
+        reward_linvel=forward_reward,
+        reward_quadctrl=-ctrl_cost,
+        reward_alive=healthy_reward,
+        x_position=com_after[0],
+        y_position=com_after[1],
+        distance_from_origin=jp.linalg.norm(com_after),
+        x_velocity=velocity[0],
+        y_velocity=velocity[1],
+    )
+
+    return state.replace(
+        pipeline_state=data, obs=obs, reward=reward, done=done
+    )
+
+  def _get_obs(
+      self, data: mjx.Data, action: jp.ndarray
+  ) -> jp.ndarray:
+    """Observes humanoid body position, velocities, and angles."""
+    position = data.qpos
+    if self._exclude_current_positions_from_observation:
+      position = position[2:]
+
+    # external_contact_forces are excluded
+    return jp.concatenate([
+        position,
+        data.qvel,
+        # data.cinert[1:].ravel(),
+        # data.cvel[1:].ravel(),
+        data.qfrc_actuator,
+    ])
 
 
 envs.register_environment('rodent', Rodent)
@@ -204,15 +203,15 @@ config = {
     "env_name": env_name,
     "algo_name": "ppo",
     "task_name": "run",
-    "num_envs": 512,
+    "num_envs": 256,
     "num_timesteps": 10_000,
     "eval_every": 1000,
     "episode_length": 500,
     "num_evals": 1000,
     "batch_size": 256,
     "learning_rate": 6e-4,
-    "terminate_when_unhealthy": False,
-    "run_platform": "local",
+    "terminate_when_unhealthy": True,
+    "run_platform": "run_ai",
 }
 
 
