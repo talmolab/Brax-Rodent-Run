e741b4d7af643334ef74c5566254d12a0d517368
Kevin Bian <kaiwenbian107@gmail.com>
2024-03-29 12:49:43 -0700

    add

--- vnl_brax/rodent_base.py
+++ vnl_brax/rodent_base.py
@@ -25,7 +25,8 @@ from dm_control.locomotion.walkers import legacy_base
 from dm_control.mujoco import wrapper as mj_wrapper
 import numpy as np
 
-_XML_PATH = 'assets/rodent_stac_optimized.xml'
+_XML_PATH = os.path.join(os.path.dirname(__file__),
+                         'assets/rodent_stac_optimized_dm_adapt.xml')
 
 _RAT_MOCAP_JOINTS = [
     'vertebra_1_extend', 'vertebra_2_bend', 'vertebra_3_twist',
