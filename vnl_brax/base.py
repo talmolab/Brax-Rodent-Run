1beff99550d602f20b33182a237a63f05a359ba2
Kevin Bian <kaiwenbian107@gmail.com>
2024-04-01 19:23:18 -0700

    added

--- vnl_brax/base.py
+++ vnl_brax/base.py
@@ -85,11 +85,11 @@ class Walker(PipelineEnv):
     # but this pass in one doesn't, it uses the default mjCONE_PYRAMIDAL, but MjModel now uses the eliptic model, so reset is needed
 
     # solver is an optimization system
-    mj_model.opt.solver = mujoco.mjtSolver.mjSOL_NEWTON #.mjSOL_CG
+    mj_model.opt.solver = mujoco.mjSOL_CG #.mjtSolver.mjSOL_NEWTON
     mj_model.opt.cone = mujoco.mjtCone.mjCONE_PYRAMIDAL # Read documentation
 
     #Iterations for solver
-    mj_model.opt.iterations = 3
+    mj_model.opt.iterations = 6
     mj_model.opt.ls_iterations = 3
 
     sys = mjcf_brax.load_model(mj_model)
