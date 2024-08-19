9ca1fec87a8346c5907a474ffd290e9f1a41fdff
Charles Zhang <charles.zhang216@gmail.com>
2024-08-19 18:20:24 -0400

    add rendering log

--- brax_rodent_run_ppo.py
+++ brax_rodent_run_ppo.py
@@ -2,65 +2,68 @@ import functools
 import jax
 from typing import Dict
 import wandb
-
+import imageio
+import mujoco
 from brax import envs
 from brax.training.agents.ppo import train as ppo
 from brax.io import model
-
+import numpy as np
 from Rodent_Env_Brax import Rodent
 
 import warnings
-warnings.filterwarnings("ignore", category=DeprecationWarning) 
+
+warnings.filterwarnings("ignore", category=DeprecationWarning)
 
 import os
 from absl import app
 from absl import flags
 
-os.environ['XLA_PYTHON_CLIENT_MEM_FRACTION'] = '0.90'
+os.environ["XLA_PYTHON_CLIENT_MEM_FRACTION"] = "0.90"
 
 FLAGS = flags.FLAGS
 
 n_gpus = jax.device_count(backend="gpu")
 print(f"Using {n_gpus} GPUs")
 
-os.environ['XLA_FLAGS'] = (
-    '--xla_gpu_enable_triton_softmax_fusion=true '
-    '--xla_gpu_triton_gemm_any=True '
+os.environ["XLA_FLAGS"] = (
+    "--xla_gpu_enable_triton_softmax_fusion=true " "--xla_gpu_triton_gemm_any=True "
 )
 
-flags.DEFINE_enum('solver', 'cg', ['cg', 'newton'], 'constraint solver')
-flags.DEFINE_integer('iterations', 4, 'number of solver iterations')
-flags.DEFINE_integer('ls_iterations', 4, 'number of linesearch iterations')
-flags.DEFINE_boolean('vision', False, 'render vision in obs')
+flags.DEFINE_enum("solver", "cg", ["cg", "newton"], "constraint solver")
+flags.DEFINE_integer("iterations", 4, "number of solver iterations")
+flags.DEFINE_integer("ls_iterations", 4, "number of linesearch iterations")
+flags.DEFINE_boolean("vision", False, "render vision in obs")
 
 config = {
     "env_name": "rodent",
     "algo_name": "ppo",
     "task_name": "run",
-    "num_envs": 1024*n_gpus,
+    "num_envs": 1024 * n_gpus,
     "num_timesteps": 500_000_000,
     "eval_every": 5_000_000,
     "episode_length": 1000,
-    "batch_size": 1024*n_gpus,
+    "batch_size": 1024 * n_gpus,
     "learning_rate": 5e-5,
     "terminate_when_unhealthy": True,
     "run_platform": "Harvard",
     "solver": "cg",
     "iterations": 6,
     "ls_iterations": 6,
-    "vision": False
+    "vision": False,
 }
 
-envs.register_environment('rodent', Rodent)
+envs.register_environment("rodent", Rodent)
 
 # instantiate the environment
 env_name = config["env_name"]
-env = envs.get_environment(env_name, 
-                           terminate_when_unhealthy=config["terminate_when_unhealthy"],
-                           solver=config['solver'],
-                           iterations=config['iterations'],
-                           ls_iterations=config['ls_iterations'],
-                           vision=config['vision'])
+env = envs.get_environment(
+    env_name,
+    terminate_when_unhealthy=config["terminate_when_unhealthy"],
+    solver=config["solver"],
+    iterations=config["iterations"],
+    ls_iterations=config["ls_iterations"],
+    vision=config["vision"],
+)
 
 # define the jit reset/step functions
 jit_reset = jax.jit(env.reset)
@@ -68,11 +71,22 @@ jit_step = jax.jit(env.step)
 
 
 train_fn = functools.partial(
-    ppo.train, num_timesteps=config["num_timesteps"], num_evals=int(config["num_timesteps"]/config["eval_every"]),
-    reward_scaling=1, episode_length=config["episode_length"], normalize_observations=True, action_repeat=1,
-    unroll_length=10, num_minibatches=64, num_updates_per_batch=8,
-    discounting=0.99, learning_rate=config["learning_rate"], entropy_cost=1e-3, num_envs=config["num_envs"],
-    batch_size=config["batch_size"], seed=0
+    ppo.train,
+    num_timesteps=config["num_timesteps"],
+    num_evals=int(config["num_timesteps"] / config["eval_every"]),
+    reward_scaling=1,
+    episode_length=config["episode_length"],
+    normalize_observations=True,
+    action_repeat=1,
+    unroll_length=10,
+    num_minibatches=64,
+    num_updates_per_batch=8,
+    discounting=0.99,
+    learning_rate=config["learning_rate"],
+    entropy_cost=1e-3,
+    num_envs=config["num_envs"],
+    batch_size=config["batch_size"],
+    seed=0,
 )
 
 import uuid
@@ -84,24 +98,76 @@ model_path = f"./model_checkpoints/{run_id}"
 run = wandb.init(
     project="vnl_debug",
     config=config,
-    notes=f"{config['batch_size']} batchsize, " + 
-        f"{config['solver']}, {config['iterations']}/{config['ls_iterations']}"
+    notes=f"{config['batch_size']} batchsize, "
+    + f"{config['solver']}, {config['iterations']}/{config['ls_iterations']}",
 )
 
 
-wandb.run.name = f"{config['env_name']}_{config['task_name']}_{config['algo_name']}_{run_id}"
+wandb.run.name = (
+    f"{config['env_name']}_{config['task_name']}_{config['algo_name']}_{run_id}"
+)
 
 
 def wandb_progress(num_steps, metrics):
     metrics["num_steps"] = num_steps
     wandb.log(metrics)
-    
+
+
 def policy_params_fn(num_steps, make_policy, params, model_path=model_path):
     os.makedirs(model_path, exist_ok=True)
     model.save_params(f"{model_path}/{num_steps}", params)
-    
+    jit_inference_fn = jax.jit(make_policy(params, deterministic=True))
+    reset_rng, act_rng = jax.random.split(jax.random.PRNGKey(0))
 
-make_inference_fn, params, _ = train_fn(environment=env, progress_fn=wandb_progress, policy_params_fn=policy_params_fn)
+    state = jit_reset(reset_rng)
+
+    rollout = [state.pipeline_state]
+    for i in range(500):
+        _, act_rng = jax.random.split(act_rng)
+        obs = state.obs
+        ctrl, extras = jit_inference_fn(obs, act_rng)
+        state = jit_step(state, ctrl)
+        rollout.append(state.pipeline_state)
+
+    # Render the walker with the reference expert demonstration trajectory
+    os.environ["MUJOCO_GL"] = "osmesa"
+    qposes_rollout = [data.qpos for data in rollout]
+    qposes_ref = qposes_rollout
+
+    mj_model = mujoco.MjModel.from_xml_path(f"./assets/rodent_pair.xml")
+
+    mj_model.opt.solver = {
+        "cg": mujoco.mjtSolver.mjSOL_CG,
+        "newton": mujoco.mjtSolver.mjSOL_NEWTON,
+    }["cg"]
+    mj_model.opt.iterations = 6
+    mj_model.opt.ls_iterations = 6
+    mj_data = mujoco.MjData(mj_model)
+
+    # save rendering and log to wandb
+    os.environ["MUJOCO_GL"] = "osmesa"
+    mujoco.mj_kinematics(mj_model, mj_data)
+    renderer = mujoco.Renderer(mj_model, height=512, width=512)
+
+    frames = []
+    # render while stepping using mujoco
+    video_path = f"{model_path}/{num_steps}.mp4"
+
+    with imageio.get_writer(video_path, fps=float(1.0 / env.dt)) as video:
+        for qpos1, qpos2 in zip(qposes_ref, qposes_rollout):
+            mj_data.qpos = np.append(qpos1, qpos2)
+            mujoco.mj_forward(mj_model, mj_data)
+            renderer.update_scene(mj_data, camera=f"close_profile")
+            pixels = renderer.render()
+            video.append_data(pixels)
+            frames.append(pixels)
+
+    wandb.log({"eval/rollout": wandb.Video(video_path, format="mp4")})
+
+
+make_inference_fn, params, _ = train_fn(
+    environment=env, progress_fn=wandb_progress, policy_params_fn=policy_params_fn
+)
 
 final_save_path = f"{model_path}/brax_ppo_rodent_run_finished"
 model.save_params(final_save_path, params)
